ed1582dbf1d4c3100031d47f9a325cb76f1118c3
Zihao Yang <zyan781@aucklanduni.ac.nz>
2017-10-05 19:06:06 +1300

    movement + camera

---
+++ Assets/Scripts/Camera/CameraThirdPerson.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraThirdPerson : MonoBehaviour {
+
+	private const float Y_ANGLE_MIN = 0f;
+	private const float Y_ANGLE_MAX = 50.0f;
+	public Transform lookAt;
+	public Transform camTransform;
+
+	private Camera cam;
+
+	private float distance = 10.0f;
+	private float currentX = 0.0f;
+	private float currentY = 0.0f;
+	private float sensitivityX = 8.0f;
+	private float sensitivityY = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		camTransform = transform;
+		cam = Camera.main;
+	}
+
+	void Update() {
+		currentX += sensitivityX * Input.GetAxis ("Mouse X");
+		currentY += sensitivityY * Input.GetAxis ("Mouse Y");
+
+		currentY = Mathf.Clamp (currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 dir = new Vector3 (0, 0, -distance);
+		Quaternion rotation = Quaternion.Euler (currentY, currentX, 0);
+		camTransform.position = lookAt.position + rotation * dir;
+		camTransform.LookAt (lookAt.position);
+
+	}
+
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 	public float speed;
+	public float turnSmoothing = 15f; // A smoothing value for turning the player.
 	private Rigidbody rigidBody;
+	Vector3 movement;
 
 	// Use this for initialization
 	void Start () {
@@ -12,10 +14,23 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		float horizontal = Input.GetAxis ("Horizontal");
-		float vertical = Input.GetAxis ("Vertical");
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
 
-		Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
-		rigidBody.AddForce (movement*speed);
+		Move (h, v);
+	
+	}
+
+
+	void Move(float h, float v) {
+
+		Vector3 movement = new Vector3(h, 0.0f, v);
+		movement = Camera.main.transform.TransformDirection(movement);
+		//make sure player always moves in same speed no matter what combination of keys
+		//this is called every with every FixedUpdate- dont want it to move 6 units every fixed update
+		//want to change it so that it is per second- multiple it by delta time. delta time is the time between each update call
+		//so if youre updating every 50th of a second, over the course of 50 50th of a second its going to move 6 units
+		movement = movement.normalized * speed * Time.deltaTime;
+		rigidBody.MovePosition (transform.position + movement);
 	}
 }
