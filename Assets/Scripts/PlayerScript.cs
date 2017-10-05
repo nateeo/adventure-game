f528c41e107c7b01c0e0bdb9d377d1a1f0354c34
Daniel <dan_wong@outlook.com>
2017-10-05 13:01:01 +1300

    Created Planet One
     - Created terrain for planet one
     - Created a basic first person view to test movement
     - Will need to implement orthographic view
    
    TODO
     - Add more detail to terrain

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public float speed;
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
+		rigidBody.AddForce (movement*speed);
+	}
+}
