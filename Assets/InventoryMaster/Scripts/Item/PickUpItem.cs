1d52a69d48c5dd4edbb5199ce66b83e55ae36e6e
Sueyeon Lee <sueyeonlee96@gmail.com>
2017-10-06 17:39:53 +1300

    Start of inventory
    Got the hotbar and inventory up, I to open inventory

---
+++ Assets/InventoryMaster/Scripts/Item/PickUpItem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PickUpItem : MonoBehaviour
+{
+    public Item item;
+    private Inventory _inventory;
+    private GameObject _player;
+    // Use this for initialization
+
+    void Start()
+    {
+		System.Diagnostics.Debug.WriteLine ("UPDATE");
+        _player = GameObject.FindGameObjectWithTag("Player");
+        if (_player != null)
+            _inventory = _player.GetComponent<PlayerInventory>().inventory.GetComponent<Inventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		System.Diagnostics.Debug.WriteLine ("PICK ME UP");
+        if (_inventory != null && Input.GetKeyDown(KeyCode.E))
+        {
+            float distance = Vector3.Distance(this.gameObject.transform.position, _player.transform.position);
+
+            if (distance <= 3)
+            {
+                bool check = _inventory.checkIfItemAllreadyExist(item.itemID, item.itemValue);
+                if (check)
+                    Destroy(this.gameObject);
+                else if (_inventory.ItemsInInventory.Count < (_inventory.width * _inventory.height))
+                {
+                    _inventory.addItemToInventory(item.itemID, item.itemValue);
+                    _inventory.updateItemList();
+                    _inventory.stackableSettings();
+                    Destroy(this.gameObject);
+                }
+
+            }
+        }
+    }
+
+}
