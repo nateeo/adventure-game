db3295cf27c81d503f645bc5638c2e6d60b0f775
MagNgo <mngo416@aucklanduni.ac.nz>
2017-10-06 16:53:27 +1300

    Added a menu screen with a start button that currently leads to the first planet, also added planet assets as part of the background for the main menu page.

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex) {
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
