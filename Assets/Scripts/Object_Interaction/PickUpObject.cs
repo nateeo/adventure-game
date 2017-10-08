bac226b78dfd72d1c4d0803c4684cb0dd44236e5
Samule Li <s.li@crimsoneducation.org>
2017-10-08 19:29:09 +1300

    Implementing puzzle set up trigger zone co author: Harry

---
+++ Assets/Scripts/Object_Interaction/PickUpObject.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpObject : MonoBehaviour {
+	
+	GameObject mainCamera;
+	GameObject playerCharacter;
+	bool carrying;
+	GameObject carriedObject;
+	public float distance;
+	public float smooth;
+	public GameObject text; 
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindWithTag("MainCamera");
+		playerCharacter = GameObject.FindWithTag ("Player");
+		text.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(carrying) {
+			carry(carriedObject);
+			checkDrop();
+			//rotateObject();
+		}
+	}
+
+	void rotateObject() {
+		carriedObject.transform.Rotate(5,10,15);
+	}
+
+	void carry(GameObject o) {
+		Debug.Log ("I am carrying");
+		Debug.Log (playerCharacter.transform.position);
+		o.transform.position = playerCharacter.transform.position + new Vector3(1,0,1);
+		Debug.Log (o.transform.position);
+		o.transform.rotation = Quaternion.identity;
+	}
+
+	void OnTriggerStay(Collider pickUpObject) {
+		if(Input.GetKeyDown (KeyCode.F)) {
+			Debug.Log("Hi there I don't know what I;m doing");
+			Pickupable p = pickUpObject.GetComponent<Pickupable>();
+			if(p != null) {
+				Debug.Log ("carry that shit?");
+				carrying = true;
+				carriedObject = p.gameObject;
+				//p.gameObject.rigidbody.isKinematic = true;
+				p.gameObject.GetComponent<Rigidbody>().useGravity = false;
+			}
+		}
+	}
+
+	void checkDrop() {
+		if(Input.GetKeyDown (KeyCode.E)) {
+			dropObject();
+		}
+	}
+
+	public void dropObject() {
+		carrying = false;
+		//carriedObject.gameObject.rigidbody.isKinematic = false;
+		carriedObject.transform.position = playerCharacter.transform.position + new Vector3(1,0,1);
+		carriedObject.gameObject.GetComponent<Rigidbody>().useGravity = true;
+		carriedObject = null;
+	}
+
+	public GameObject getCarriedObject() {
+		return carriedObject;
+	}
+}
---
+++ Assets/Scripts/Object_Interaction/TriggerRiverEvent.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerRiverEvent : MonoBehaviour {
+
+	public GameObject text;
+	public GameObject log1;
+	PickUpObject pickUpShit;
+	GameObject playerController;
+	Collider playerCollider;
+
+	// Use this for initialization
+	void Start () {
+
+		playerController = GameObject.FindWithTag ("Player");
+		pickUpShit = playerController.GetComponent<PickUpObject>();
+		playerCollider = playerController.GetComponent<Collider>();
+		text.SetActive (false);
+		log1.SetActive (false);
+	}
+
+	
+	// Update is called once per frame
+	void OnTriggerStay(Collider player) {
+		Debug.Log (player);
+		if(player == playerCollider) {
+			text.SetActive (true);
+			GameObject go = pickUpShit.getCarriedObject ();
+			if(go != null) {
+				Log l = go.GetComponent<Log>();
+				if (l != null) {
+					if (Input.GetKeyDown (KeyCode.G)) {
+						pickUpShit.dropObject ();
+						go.SetActive (false);
+						log1.SetActive (true);
+					}
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit() {
+		text.SetActive(false);
+	}
+}
