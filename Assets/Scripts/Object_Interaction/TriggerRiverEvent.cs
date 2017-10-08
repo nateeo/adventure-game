6ad5d2b09a27bf1390ad4617af7c129bbcd6b964
Samule Li <s.li@crimsoneducation.org>
2017-10-08 16:34:32 +1300

    TriggerRiverEvent

--- Assets/Scripts/Object_Interaction/TriggerRiverEvent.cs
+++ Assets/Scripts/Object_Interaction/TriggerRiverEvent.cs
@@ -6,28 +6,34 @@ public class TriggerRiverEvent : MonoBehaviour {
 	public GameObject text;
 	public GameObject log1;
 	PickUpObject pickUpShit;
+	GameObject playerController;
+	Collider playerCollider;
 
 	// Use this for initialization
 	void Start () {
 
-		pickUpShit = GameObject.FindWithTag ("Player").GetComponent<PickUpObject>();
+		playerController = GameObject.FindWithTag ("Player");
+		pickUpShit = playerController.GetComponent<PickUpObject>();
+		playerCollider = playerController.GetComponent<Collider>();
 		text.SetActive (false);
 		log1.SetActive (false);
 	}
+
 	
 	// Update is called once per frame
 	void OnTriggerStay(Collider player) {
-
-		GameObject go = pickUpShit.getCarriedObject ();
-		if(go != null) {
-			
-			Log l = go.GetComponent<Log>();
-			if (l != null) {
-				text.SetActive (true);
-				if (Input.GetKeyDown (KeyCode.G)) {
-					pickUpShit.dropObject ();
-					go.SetActive (false);
-					log1.SetActive (true);
+		Debug.Log (player);
+		if(player == playerCollider) {
+			text.SetActive (true);
+			GameObject go = pickUpShit.getCarriedObject ();
+			if(go != null) {
+				Log l = go.GetComponent<Log>();
+				if (l != null) {
+					if (Input.GetKeyDown (KeyCode.G)) {
+						pickUpShit.dropObject ();
+						go.SetActive (false);
+						log1.SetActive (true);
+					}
 				}
 			}
 		}
