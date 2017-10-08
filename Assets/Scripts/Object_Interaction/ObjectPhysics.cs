10002cc43ea7df9a4d97c889de00d9df41eac1ef
Samule Li <s.li@crimsoneducation.org>
2017-10-08 15:24:44 +1300

    Implememnted game mechanics

---
+++ Assets/Scripts/Object_Interaction/ObjectPhysics.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// moves rigidbody objects around with Character Controller object
+/// <remarks>
+/// C# conversion of script used in this video:
+///     http://www.youtube.com/watch?v=GVseGuTJf24
+/// place on Character Controller object
+/// un-comment 'RequireComponent' line to allow for
+///     dependencies on Character Controller component
+/// </remarks>
+/// </summary>
+//[RequireComponent(typeof(CharacterController))]
+public class ObjectPhysics : MonoBehaviour
+{
+
+	public float pushForce = 2.0f;
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		Rigidbody body = hit.collider.attachedRigidbody;
+
+		//checking whether rigidbody is either non-existant or kinematic
+		if (body == null || body.isKinematic)
+			return;
+
+		if (hit.moveDirection.y < -.3f)
+			return;
+
+		//set up push direction for object
+		Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+		//apply push force to object
+		body.velocity = pushForce * pushDirection;
+	}
+}
---
+++ Assets/Scripts/Object_Interaction/TriggerRiverEvent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerRiverEvent : MonoBehaviour {
+
+	public GameObject text;
+	public GameObject log1;
+	PickUpObject pickUpShit;
+
+	// Use this for initialization
+	void Start () {
+
+		pickUpShit = GameObject.FindWithTag ("Player").GetComponent<PickUpObject>();
+		text.SetActive (false);
+		log1.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay(Collider player) {
+
+		GameObject go = pickUpShit.getCarriedObject ();
+		if(go != null) {
+			
+			Log l = go.GetComponent<Log>();
+			if (l != null) {
+				text.SetActive (true);
+				if (Input.GetKeyDown (KeyCode.G)) {
+					pickUpShit.dropObject ();
+					go.SetActive (false);
+					log1.SetActive (true);
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit() {
+		text.SetActive(false);
+	}
+}
