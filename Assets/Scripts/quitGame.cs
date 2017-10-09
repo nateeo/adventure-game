1cdf7e427d85b3dc239fa735bd762736343a8eb1
MagNgo <mngo416@aucklanduni.ac.nz>
2017-10-09 17:38:59 +1300

    added functionality to the exit button on the main menu, also fixed some spelling mistakes

--- Assets/Scripts/quitGame.cs
+++ Assets/Scripts/quitGame.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class quitGame : MonoBehaviour {
 
+	//code to quit out of game using the exit button
 	public void exitGame() {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
 		Application.Quit ();
+		#endif
 	}
 }
 	
