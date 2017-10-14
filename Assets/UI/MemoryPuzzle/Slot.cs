5716e3b711b58a77b1e17b3cf08f2bdf20dfec38
victorlian <vlia679@aucklanduni.ac.nz>
2017-10-14 17:24:14 +1300

    Basic drag and drop for memory puzzle
    Including:
    1. UI slots and images.
    2. scripts for drag and drop

---
+++ Assets/UI/MemoryPuzzle/Slot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+/*This script should be attached to a slot. 
+* This will allow a slot to receive an UI element through dropping.
+* 
+* Author: Victor
+*/
+public class Slot : MonoBehaviour, IDropHandler{
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    #region IDropHandler implementation
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            DragHandler.itemBeingDragged.transform.SetParent(transform);
+        }
+    }
+    #endregion
+}
