cb99bc8ac2b1d67bfaf94cbc560c1a2fe73fccb9
Daniel <dan_wong@outlook.com>
2017-10-17 16:35:25 +1300

    Game Objective marker working

--- Assets/Scripts/GameObjective.cs
+++ Assets/Scripts/GameObjective.cs
@@ -12,11 +12,11 @@ public class ObjectiveMarker : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		var rotationAngle = Quaternion.LookRotation ( target.position - transform.position); // we get the angle has to be rotated
-		transform.rotation = Quaternion.Slerp ( transform.rotation, rotationAngle, Time.deltaTime * damp); // we rotate the rotationAngle 
-
 		//Get the distance to the object marker
 		float distance = Vector3.Distance (target.position, transform.position);
+
+		var rotationAngle = Quaternion.LookRotation ( target.position - transform.position); // we get the angle has to be rotated
+		transform.rotation = Quaternion.Slerp ( transform.rotation, rotationAngle, Time.deltaTime * damp); // we rotate the rotationAngle 
 
 		//If the scale is less than 1, keep it at 1, if the scale is greater than 30, keep it at 30
 		float transformScale = (distance / 30) < 1 ? 1 : (distance / 30);
@@ -27,9 +27,7 @@ public class ObjectiveMarker : MonoBehaviour {
 
 		//Make the marker disappear when you are within a certain distance
 		if (distance <= 10) {
-			gameObject.active = false;
-		} else {
-			gameObject.active = true;
+			transform.localScale = new Vector3 (0, 0, 0);
 		}
 	}
 }
