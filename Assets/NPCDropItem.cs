ffee8e148b8cb40375330aae8c01e8caf4d5723f
Nathan Hur <hur.nathan@gmail.com>
2017-10-19 14:44:52 +1300

    Only trigger when player is in range to interact

--- Assets/NPCDropItem.cs
+++ Assets/NPCDropItem.cs
@@ -7,16 +7,17 @@ public class NPCDropItem : MonoBehaviour
 	public GameObject item;
 	public int keyPressTrigger;
 	private int keyPressed = 0;
-
+	private GameObject _player;
 	void Start()
 	{
+		_player = GameObject.FindGameObjectWithTag("Player");
 		item.SetActive(false);
 	}
 
 	public void Update()
 	{
-		// Check if F has been pressed, only if the item hasn't been picked up and is still hidden
-		if (item != null && !item.activeSelf && Input.GetKeyDown(KeyCode.F) == true)
+		// Check if F has been pressed, only if the item hasn't been picked up and is still hidden and user is within range
+		if (Input.GetKeyDown(KeyCode.F) && Vector3.Distance(this.transform.position, _player.transform.position) <= 2.2f && item != null && !item.activeSelf)
 		{
 			keyPressed++;
 			// Check if the conversation between the user and NPC is finished
