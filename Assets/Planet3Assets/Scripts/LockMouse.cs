4f8dee1d5c0d9f808b61cdc48217d3dd5b7f8be6
Daniel <dan_wong@outlook.com>
2017-10-20 18:14:40 +1300

    Added edges to walls to show more defined walls

---
+++ Assets/Planet3Assets/Scripts/LockMouse.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
