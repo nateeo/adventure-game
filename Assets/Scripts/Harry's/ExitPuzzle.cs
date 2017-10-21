e7b95e416633fdcf1947f1f7a4e24fdb7e7643cb
harrylimp <harrylim.liverpool@gmail.com>
2017-10-22 02:13:39 +1300

    Added puzzle generation scripts
    Generates partial data for the two puzzles within the game.

---
+++ Assets/Scripts/Harry's/ExitPuzzle.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExitPuzzle : MonoBehaviour
+{
+
+    public GameObject trueClue1;
+    public GameObject trueClue2;
+    public GameObject trueClue3;
+    public GameObject trueClue4;
+    public GameObject falseClue1;
+    public GameObject falseClue2;
+    public GameObject falseClue3;
+    public GameObject falseClue4;
+    public GameObject Plugman1;
+
+    private List<GameObject> trueClues = new List<GameObject>();
+    private List<GameObject> falseClues = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+        initialiseClues();
+        hideClues();
+    }
+
+    private void initialiseClues()
+    {
+        trueClues.Add(trueClue1);
+        trueClues.Add(trueClue2);
+        trueClues.Add(trueClue3);
+        trueClues.Add(trueClue4);
+        falseClues.Add(falseClue1);
+        falseClues.Add(falseClue2);
+        falseClues.Add(falseClue3);
+        falseClues.Add(falseClue4);
+    }
+
+    private void hideClues()
+    {
+        for (int i = 0; i < trueClues.Count; i++)
+        {
+            trueClues[i].SetActive(false);
+            falseClues[i].SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Entered");
+        Plugman1.transform.position = trueClue1.transform.position;
+
+    }
+}
---
+++ Assets/Scripts/Harry's/GenerateNumbers.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateNumbers : MonoBehaviour {
+
+    public GameObject numberWall1;
+    public GameObject numberWall2;
+    public GameObject numberWall3;
+    public GameObject numberWall4;
+    private List<GameObject> walls;
+    private string passcode;
+
+	// Use this for initialization
+	void Start () {
+        walls = initializeWalls();
+
+        List<int> numbers = generateNumbers(walls.Count);
+        passcode = createPasscode(numbers);
+        plotWalls(walls, numbers);
+	}
+
+    private List<GameObject> initializeWalls()
+    {
+        List<GameObject> tempWalls = new List<GameObject>();
+        tempWalls.Add(numberWall1);
+        tempWalls.Add(numberWall2);
+        tempWalls.Add(numberWall3);
+        tempWalls.Add(numberWall4);
+        return tempWalls;
+    }
+
+    public void hideWalls()
+    {
+        for (int i = 0; i < walls.Count; i++)
+        {
+            walls[i].SetActive(false);
+        }
+    }
+
+    public void showWalls()
+    {
+        for (int i = 0; i < walls.Count; i++)
+        {
+            walls[i].SetActive(true);
+        }
+    }
+
+    public string getPasscode()
+    {
+        return passcode;
+    }
+
+    private List<int> generateNumbers(int numberOfWalls)
+    {
+        List<int> numbers = new List<int>();
+        for (int i = 0; i < numberOfWalls; i++)
+        {
+            numbers.Add(Random.Range(0, 10));
+        }
+        return numbers;
+    }
+
+    private void plotWalls(List<GameObject> walls, List<int> numbers)
+    {
+        Debug.Log("HELLOOOO");
+        // Plotting a single wall
+        for (int i = 0; i < walls.Count; i++)
+        {
+            GameObject wall = walls[i];
+            TextMesh t = wall.AddComponent<TextMesh>();
+            t.text = numbers[i].ToString();
+            //t.fontSize = 150;
+
+            Debug.Log("KMS");
+
+            t.transform.localEulerAngles += new Vector3(0, 90, 0);
+            //t.transform.position = wall.transform.position;
+            //t.transform.position = GameObject.Find("NumberWallRight" + (i + 1)).transform.position;
+            Debug.Log(t.transform.position);
+        }
+    }
+
+    private string createPasscode(List<int> numbers)
+    {
+        numbers.Sort();
+        string passcode = "";
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            passcode += numbers[i].ToString();
+        }
+        return passcode;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.transform.position);
+        Debug.Log(other.gameObject.name);
+        gameObject.transform.Rotate(-180, 0, 0);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exiting");
+        gameObject.transform.Rotate(180, 0, 0);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Entered");
+    }
+}
