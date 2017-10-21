fa32bcea1ee66718ec8eca9ad9a25dd69afe498b
MagNgo <mngo416@aucklanduni.ac.nz>
2017-10-22 12:02:02 +1300

    added layout of the puzzle as well as a camera and a script for bringing up the camera. Still need to edit player script

---
+++ Assets/mapCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mapCamera : MonoBehaviour {
+	public GameObject camera;
+	bool visible;
+
+	// Use this for initialization
+	void Start () {
+		camera.SetActive(false);
+		visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.P) && visible == false) {
+			camera.SetActive(true);
+			Cursor.lockState = CursorLockMode.Locked;
+			visible = true;
+		} else if (Input.GetKeyDown (KeyCode.P) && visible == true) {
+			camera.SetActive(false);
+			visible = false;
+			Cursor.lockState = CursorLockMode.None;
+
+
+		}
+	}
+}
---
+++ Assets/onCollision.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onCollision : MonoBehaviour {
+	private Rigidbody rb;
+	bool icy = false;
+	bool icyMovement;
+	private GameObject playerCharacter;
+	PlayerScript player;
+
+	void Update() {
+		playerCharacter = GameObject.FindWithTag ("Player");
+		PlayerScript player = playerCharacter.AddComponent<PlayerScript> ();
+
+
+		if (Input.GetKeyDown (KeyCode.P) && icy == false) {
+			Debug.Log ("hi icy");
+			icy = true;
+		} else if (Input.GetKeyDown (KeyCode.P) && icy == true) {
+			Debug.Log ("bye icy");
+			icy = false;
+		}
+			
+	}
+	void OnCollisionEnter ( Collision collision) {
+		Debug.Log ("urgggghhh");
+		Debug.Log (icy);
+
+
+		if (icy == false) {
+			Debug.Log ("not icy");
+
+			return;
+		}
+		
+		player.icyMovement = true;
+		Debug.Log ("icyyyyy");
+
+	}
+
+}
