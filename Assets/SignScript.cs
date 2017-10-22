377efec592aa5231c64fbe8403584a543ffce1bf
Nathan Hur <hur.nathan@gmail.com>
2017-10-22 20:33:54 +1300

    add all signs

--- Assets/SignScript.cs
+++ Assets/SignScript.cs
@@ -4,26 +4,29 @@ using System.Collections;
 
 public class SignScript : MonoBehaviour {
 
-	// to display on the user UI
+	// to display on the user UI, singleton signText
 	public Text signText;
 	public string text;
 	public float signDistance;
+	GameObject player;
 	static int inRange;
 	bool activeSelf;
 
 	void Start() {
+		player = GameObject.FindGameObjectWithTag ("Player");
 		activeSelf = false;
 	}
 
 	void Update() {
-		
-		if (Vector3.Distance (transform.position, Camera.main.transform.position) < signDistance) {
+
+		if (Vector3.Distance (transform.position, player.transform.position) < signDistance) {
 			if (!activeSelf) {
 				activeSelf = true;
 				inRange++;
 				signText.text = text;
 			}
 		} else {
+			// disable the signText if no other signs are in range
 			if (activeSelf) {
 				activeSelf = false;
 				inRange--;
