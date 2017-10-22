79e160db544356d8558b7f4d62694cc01c181660
Daniel Wong <dan_wong@outlook.com>
2017-10-22 20:57:02 +1300

    Made scene change for puzzle. Fixed materials in planet

---
+++ Assets/Planet3Assets/Scripts/PuzzleEnding.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * This handles the scenechange at the end of the puzzle
+ */
+public class PuzzleEnding : MonoBehaviour {
+	void OnTriggerStay(Collider collide) {
+		Application.LoadLevel("Planet3");
+	}
+}
