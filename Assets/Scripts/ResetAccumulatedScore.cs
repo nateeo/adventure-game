f788b0fb756498df3c854936ce260c557a52a50e
Nathan Hur <hur.nathan@gmail.com>
2017-10-24 05:12:37 +1300

    fix main menu, extend third planet dialogue

--- Assets/Scripts/ResetAccumulatedScore.cs
+++ Assets/Scripts/ResetAccumulatedScore.cs
@@ -5,6 +5,8 @@ public class ResetAccumulatedScore : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = true;
 		PlayerPrefs.SetInt ("AccumulatedScore", 0);
 	}
 }
