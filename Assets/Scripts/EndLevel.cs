f47d76ddeb4d53b71caf667b25a0621d7f32b0b4
Nathan Hur <hur.nathan@gmail.com>
2017-10-23 21:06:31 +1300

    scene changing, update planet 3

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			SceneManager.LoadScene (2);
+		}
+	}
+}
